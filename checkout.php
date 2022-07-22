899e33675810b0ebd08271fc9186122aefe0e819
boctulus <boctulus@gmail.com>
2022-07-22 13:56:14 +0200

    .

--- checkout.php
+++ checkout.php
@@ -18,13 +18,14 @@ require_once __DIR__ . '/libs/Arrays.php';
     Hook checkout
 */
 
+add_action( 'woocommerce_before_checkout_form', 'scripts_ezfarma_checkout' );
 
-if ($_SERVER['REQUEST_URI'] == '/checkout/'):
-	?>
-	<script>
+function scripts_ezfarma_checkout(){
+    ?>
+    <script>
         const SITE_URL = '<?= get_site_url(); ?>';
 
-		function getBase64(file) {
+        function getBase64(file) {
             return new Promise((resolve, reject) => {
                 const reader = new FileReader();
                 reader.readAsDataURL(file);
@@ -74,18 +75,46 @@ if ($_SERVER['REQUEST_URI'] == '/checkout/'):
                     })
                     .done(function(data) {
                         console.log(data);
+                        addNotice("Receta recibida", "info", "ef_checkout_messages");
                         console.log("success");
                     })
                     .fail(function(data) {
                         console.log(data);
+                        addNotice("Error al recibir receta", "danger", "ef_checkout_messages");
                         console.log("error");
                     });
                 }
             );
         }
-	</script>
-<?php
-endif;
+    </script>
+    <?php
+}
+
+
+function table_gen(Array $names){
+    $trs = [];
+
+    foreach ($names as $name){
+        $trs[] = 
+        "<tr>
+            <th scope=\"row\">$name</th>
+        </tr>";
+    }
+
+    $trstr = implode("\r\n", $trs);
+    
+    return '<table class="dcf-table dcf-table-responsive dcf-table-bordered dcf-table-striped dcf-w-100%">
+        <caption>Medicamentos con receta</caption>
+        <thead>
+            <tr>
+                <td>Nombre</td>
+            </tr>
+        </thead>
+        <tbody>'
+            .$trstr.
+        '</tbody>
+    </table>';
+}
 
 
 add_action( 'woocommerce_after_order_notes', 'add_ezfarma_custom_fields' );
@@ -97,11 +126,10 @@ function add_ezfarma_custom_fields($checkout)
     $req_receta_ay = [];
     foreach ($items as $item => $values)
     {
-        $qty = $values['quantity'];
-        $pid = $values['product_id'];
-        $sku = $values['data']->get_sku();
-
-        //dd($qty, $pid);
+        $qty  = $values['quantity'];
+        $pid  = $values['product_id'];
+        $sku  = $values['data']->get_sku();
+        $name = $values['data']->get_name();
 
         if (EasyFarma::isSkuPlus($sku)){
             $pid = EasyFarma::getNonPlusProductId($sku);
@@ -110,28 +138,41 @@ function add_ezfarma_custom_fields($checkout)
         $req = Products::getMeta($pid, 'requiere_receta');
 
         if ($req == 'Si' || $req == 'Sí'){
-            $req_receta_ay[] = $pid;
+            $req_receta_ay[] = [
+                'pid'  => $pid,
+                'name' => $name
+            ];
         }
     }
 
-    dd($req_receta_ay, 'REQUIEREN RECETA');
+    if (empty($req_receta_ay)){
+        return;
+    }
+
+    $names = array_column($req_receta_ay, 'name');
 
     ?>
     <h3>Receta adjunta</h3>
 
-    <label><strong>Hay medicamentos que requieren que adjunte receta médica.</strong></label>
+    <?php
+        echo table_gen($names);
+    ?>
+
+    <label><strong>* Hay medicamentos que requieren que adjunte receta médica.</strong></label>
 
     <p></p>
-    <p class="form-row form-row-wide validate-required validate-phone">
+    <p style="margin-top:2em" class="form-row form-row-wide validate-required validate-phone">
         <label for="file" class="">Adjunto&nbsp;
             <!-- abbr class="required" title="obligatorio">*</abbr></label -->
             <span class="woocommerce-input-wrapper">
-                <input type="file" class="input-text " name="file" id="file" accept='image/*' />
+                <input type="file" class="input-text " name="file" id="file" accept="image/*,application/pdf" /> <!-- accept='image/*' -->
             </span>
     </p>
 
-    <button onclick="send_file();" type="button">Adjuntar</button>
+    <button onclick="send_file();" type="button" id="adjuntar_recetas">Adjuntar</button>
+    <p></p>
 
+    <div style="margin-top:1em" id="ef_checkout_messages"></div>
     <?php
 }
 
--- easyfarmadespachos.php
+++ easyfarmadespachos.php
@@ -55,6 +55,70 @@ function my_css_enqueues()
 
 		wp_register_style('ef_main',  plugin_dir_url(__FILE__) . '/assets/css/ef_styles.css');
 		wp_enqueue_style('ef_main');
+
+		?>
+			<script>
+				function addNotice(message, type = 'info', id_container = 'alert_container', replace = false){
+					let types = ['info', 'danger', 'warning', 'success'];
+
+					if (jQuery.inArray(type, types) == -1){
+						throw "Tipo de notificación inválida para " + type;
+					}
+
+					if (message === ""){
+						throw "Mensaje de notificación no puede quedar vacio";
+						return;
+					}
+
+					let alert_container  = document.getElementById(id_container);
+				
+					if (replace){
+						alert_container.innerHTML = '';
+					}
+
+					let code = (new Date().getTime()).toString();
+					let id_notice = "notice-" + code;
+					let id_close  = "close-"  + code;
+
+					div = document.createElement('div');			
+					div.innerHTML = `
+					<div class="alert alert-${type} alert-dismissible fade show mt-3" role="alert" id="${id_notice}">
+						<span>
+							${message}
+						</span>
+						<button type="button" class="btn-close notice" data-bs-dismiss="alert" aria-label="Close" id="${id_close}"></button>
+					</div>`;
+
+					alert_container.classList.add('mt-5');
+					alert_container.prepend(div);
+
+					document.getElementById(id_close).addEventListener('click', () => {
+						let cnt = document.querySelectorAll('button.btn-close.notice').length -1;
+						if (cnt == 0){
+							alert_container.classList.remove('mt-5');
+							alert_container.classList.add('mt-3');
+						}
+					});
+
+
+					return id_notice;
+				}
+
+				function hideNotice(id_container = 'alert_container', notice_id = null){
+					if (notice_id == null){
+						let div  = document.querySelector(`div#${id_container}`);
+						div.innerHTML = '';
+						alert_container.classList.remove('mt-5');
+					} else {
+						document.getElementById(notice_id).remove();
+					}
+				}
+
+				function clearNotices(id_container = 'alert_container'){
+					hideNotice(id_container);
+				}
+			</script>
+		<?php
 	//}
 }
 
